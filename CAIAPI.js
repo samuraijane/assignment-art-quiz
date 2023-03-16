a183fae167c071a99ccb296085066f289b17f766
Maggie <zach.benton.27@gmail.com>
2023-03-16 16:28:36 -0400

    push for review

--- CAIAPI.js
+++ CAIAPI.js
@@ -1,9 +1,9 @@
 const body = document.getElementById('body');
 const btn = document.getElementById('btn');
 
-const getImgInfoFunc = async () => {
+const getImgInfoFunc = async (dataUrl) => {
   //fetch request function 
-  const response = await fetch();
+  const response = await fetch(dataUrl);
   //converting into json
   const data = await response.json();
   //gathering iiif http link
@@ -23,30 +23,33 @@ const buildImgUrlFunc = async () => {
   let img = document.createElement('img');
   //using temporal literate syntax to build url from necessary components.
   img.src = `${iiif}/${imageId}/full/843,/0/default.jpg`;
-  console.log(img.src)
+  // console.log(img.src);
   body.append(img);
 }
 
-function getApiUrl() {
-  const apiUrl = [];
-  for (let i = 0; i <= 3; i++) {
-    let dataUrl = `https://api.artic.edu/api/v1/artworks?page=${i}`;
-    apiUrl.push(dataUrl);
+const getApiUrl = () => {
+  // const apiUrl = [];
+  for (let i = 1; i < 5; i++) {
+    let dataUrl = `https://api.artic.edu/api/v1/artworks?page=${i}&fields=image_id,artist_title,id,iiif_url`;
+    console.log(dataUrl);
+    getImgInfoFunc(dataUrl);
   }
-  console.log(apiUrl);
-  return apiUrl;
+  // console.log(apiUrl);
+  // return apiUrl;
 }
 
-const pickApiUrl = () => {
-  const apiUrl = getApiUrl();
-  for (let i = 0; i <= apiUrl.length; i++){
-    
-  }
-
-}
+// const pickApiUrl = () => {
+//   const apiUrl = getApiUrl();
+//   for (let i = 0; i <= 3; i++){
+//     let indUrl = apiUrl.pop();
+//     console.log(indUrl);
+//     getImgInfoFunc(indUrl);
+//     // let numIndUrl = indUrl
+//   }
+// }
 //to total the number of points create a final string interp saying take sum of total points and add a zero to it (e.g. `your score is ${sum}0`)
 
-btn.addEventListener('click', getApiUrl);
+btn.addEventListener('click', buildImgUrlFunc);
 // const iiif = data.config.iiif_url
 
 // "https://api.artic.edu/api/v1/artworks?page=1&page=100"
