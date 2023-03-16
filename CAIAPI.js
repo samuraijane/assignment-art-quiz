e6221d303a865a4d85174e246f6faaeabb818378
Maggie <zach.benton.27@gmail.com>
2023-03-16 17:28:41 -0400

    update for more review

--- CAIAPI.js
+++ CAIAPI.js
@@ -2,18 +2,23 @@ const body = document.getElementById('body');
 const btn = document.getElementById('btn');
 
 const getImgInfoFunc = async (dataUrl) => {
+  const imgIdArr = [];
   //fetch request function 
   const response = await fetch(dataUrl);
   //converting into json
   const data = await response.json();
+  console.log(data);
   //gathering iiif http link
   const iiif = data.config.iiif_url;
-  //getting img id
-  const imageId = data.data.image_id;
-  //cacheing both
-  return {iiif, imageId};
-  // use this format to build img urls
-  // https://www.artic.edu/iiif/2/{identifier}/full/843,/0/default.jpg
+  //separating each imgs info
+  const indImgInfo = () => {
+    data.data.forEach(image_id => 
+      imgIdArr.push(image_id)
+    );
+    return imgIdArr;
+  }
+  console.log(imgIdArr);
+  return {indImgInfo, iiif};
 }
 
 const buildImgUrlFunc = async () => {
@@ -27,12 +32,13 @@ const buildImgUrlFunc = async () => {
   body.append(img);
 }
 
-const getApiUrl = () => {
+const getApiUrl = async () => {
   // const apiUrl = [];
   for (let i = 1; i < 5; i++) {
     let dataUrl = `https://api.artic.edu/api/v1/artworks?page=${i}&fields=image_id,artist_title,id,iiif_url`;
-    console.log(dataUrl);
-    getImgInfoFunc(dataUrl);
+    // console.log(dataUrl);
+    await getImgInfoFunc(dataUrl);
+    // buildImgUrlFunc()
   }
   // console.log(apiUrl);
   // return apiUrl;
@@ -49,7 +55,7 @@ const getApiUrl = () => {
 // }
 //to total the number of points create a final string interp saying take sum of total points and add a zero to it (e.g. `your score is ${sum}0`)
 
-btn.addEventListener('click', buildImgUrlFunc);
+btn.addEventListener('click', getApiUrl);
 // const iiif = data.config.iiif_url
 
 // "https://api.artic.edu/api/v1/artworks?page=1&page=100"
